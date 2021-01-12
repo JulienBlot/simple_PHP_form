9baa113d68a0412d36d274e01f2d884e32531d4a
JulienBlot <52584046+JulienBlot@users.noreply.github.com>
2021-01-12 19:07:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <title>Formulaire en PHP</title>
-    <!-- Nous pourrions insérer d'autres méta-données ici -->
+    <!-- We could insert more metadata here-->
 </head>
 
 <body>
@@ -14,12 +14,12 @@
         <!-- employer la méthode POST pour ne pas avoir les restrictions du GET (données à envoyer au serveur sont écrites directement dans l'URL) ainsi que d'assurer la confidentialité nécessaire et davantage de flexibilité -->
 
         <p>
-            <label for="prenom"> Entrez votre prénom : </label>
+            <label for="prenom"> Enter your first name: </label>
             <input type="text" name="prenom" id="prenom" /> <!-- le name prénom sera la clé, et la valeur associée à cette clé sera la valeur envoyée par l'utilisateur --> 
         </p>
 
         <p>
-            <input type="submit" value="Envoyer" />
+            <input type="submit" value="Submit" />
         </p>
 
     </form>
